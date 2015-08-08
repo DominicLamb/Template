e61876eb065427cf5e6d47ce0120f65ad964cf72
Schaflein <contact@dynamically-typed.com>
2015-08-08 21:04:15 +0100

    Add tests for simple argument parsing.

--- tests/RendererTest.php
+++ tests/RendererTest.php
@@ -21,7 +21,6 @@ class RendererTest extends PHPUnit_Framework_TestCase {
 		$this->assertSame('', $string);
 	}
     public function testRenderSimpleString() {
-		$path = $this->template->getPath();
 		$this->template->addFile('test');
 		$this->tree->add('simpleString', 'root');
 		$this->tree->setRoot('root');
@@ -31,7 +30,6 @@ class RendererTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testRenderRepeatedSimpleString() {
-		$path = $this->template->getPath();
 		$this->template->addFile('test');
 		$this->tree->add('simpleString', 'root');
 		$this->tree->add('simpleString', 'root');
@@ -41,5 +39,69 @@ class RendererTest extends PHPUnit_Framework_TestCase {
 		$string = $this->renderer->render($this->tree, $this->template);
 		$this->assertSame('This is a simple stringThis is a simple stringThis is a simple string', $string);
     }
+	public function testAddNamespace() {
+		$this->renderer->addNamespace('test', 'TEST');
+		$namespace = $this->renderer->getNamespace('test');
+		$this->assertCount(2, $namespace);
+	}
+
+	public function testHandleSimpleFirstArg() {
+		$this->template->addFile('test');
+		$this->tree->add('simpleFirstArg', 'root', 'blue');
+		$this->tree->setRoot('root');
+		$string = $this->renderer->render($this->tree, $this->template);
+		$this->assertSame('Blue is blue', $string);
+	}
+
+	public function testHandleSimpleJustArg() {
+		$this->template->addFile('test');
+		$this->tree->add('simpleJustArg', 'root', 'blue');
+		$this->tree->setRoot('root');
+		$string = $this->renderer->render($this->tree, $this->template);
+		$this->assertSame('blue', $string);
+	}
+
+	public function testHandleSimpleMultipleArg() {
+		$this->template->addFile('test');
+		$this->tree->add('simpleMultipleArg', 'root', 'blue', 'red', 'green');
+		$this->tree->setRoot('root');
+		$string = $this->renderer->render($this->tree, $this->template);
+		$this->assertSame('blue and red and green', $string);
+	}
+
+
+
+
+	public function testHandleBadTokenStart() {
+		$this->template->addFile('test');
+		$this->tree->add('badTokenStart', 'root', 'blue');
+		$this->tree->setRoot('root');
+		$string = $this->renderer->render($this->tree, $this->template);
+		$this->assertSame('Blue is ARG1>', $string);
+	}
+
+	public function testHandleBadTokenEnd() {
+		$this->template->addFile('test');
+		$this->tree->add('badTokenEnd', 'root', 'blue');
+		$this->tree->setRoot('root');
+		$string = $this->renderer->render($this->tree, $this->template);
+		$this->assertSame('Blue is <ARG1', $string);
+	}
+
+	public function testHandleBadTokenIndex() {
+		$this->template->addFile('test');
+		$this->tree->add('badTokenIndex', 'root', 'blue');
+		$this->tree->setRoot('root');
+		$string = $this->renderer->render($this->tree, $this->template);
+		$this->assertSame('Blue is <ARG>', $string);
+	}
+
+	public function testHandleNestedFirstArg() {
+		$this->template->addFile('test');
+		$this->tree->add('nestedFirstArg', 'root', 'blue');
+		$this->tree->setRoot('root');
+		$string = $this->renderer->render($this->tree, $this->template);
+		$this->assertSame('Blue is <ARblueG1>', $string);
+	}
 }
 ?>
