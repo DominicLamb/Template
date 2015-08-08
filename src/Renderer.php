13084863f820b7d0f1ffe6ff1854177e4b4727a1
Schaflein <contact@dynamically-typed.com>
2015-08-08 21:01:28 +0100

    Add basic template argument parsing.

--- src/Renderer.php
+++ src/Renderer.php
@@ -4,12 +4,24 @@ use \RuntimeException;
 
 class Renderer {
     private $namespaces = array();
-	public function addNamespace($name, $key) {
+	public function addNamespace($name, $key, $data = array()) {
 		$this->namespaces[$name] = array(
 			'key' => $key,
-			'data' => array()
+			'data' => $data
 		);
 	}
+	public function addNamespaceData($namespace, $key, $value) {
+		if(!isset($this->namespaces[$name])) {
+			throw new RuntimeException('Namespace has not been loaded: ' . $namespace);
+		}
+	}
+	public function getNamespace($name) {
+		if(!isset($this->namespaces[$name])) {
+			throw new RuntimeException('Namespace not found: ' . $name);
+		}
+		return $this->namespaces[$name];
+	}
+
 	public function render(RenderTree $tree, Template $template) {
         $templateString = '';
 		$root = $tree->getRoot();
@@ -26,20 +38,73 @@ class Renderer {
 		$templateString = $this->renderNode($rootNode, $template);
         return $templateString;
 	}
+
 	private function renderNode($node, $template) {
 		$templateString = '';
 		foreach($node as $element) {
-			$templateString .= $this->renderNamespaces($template[$element[0]]);
+			$elementString = $this->handleArgs($template[$element[0]], $element[2]);
+			$templateString .= $this->renderNamespaces($elementString);
 		}
 		return $templateString;
 	}
+
 	private function renderNamespaces($element) {
 		foreach($this->namespaces as $namespace) {
 			$this->renderNamespace($element, $namespace);
 		}
 		return $element;
 	}
+	private function handleArgs($string, $args) {
+		$namespace = 'ARG';
+		$outString = $string;
+		$nameLength = strlen($namespace);
+		$pos = 0;
+		do {
+			$pos = strpos($outString, $namespace, $pos + 1);
+			if($pos !== false) {
+				$index = $outString[$pos + $nameLength];
+				if(is_numeric($index) && isset($args[$index - 1])) {
+					$outString = $this->handleArg($outString, $pos, $args[$index - 1], $index);
+				}
+			}
+		}
+		while($pos !== false);
+
+		return $outString;
+	}
+	private function handleArg($string, $pos, $arg, $index) {
+		$outString = $string;
+		$tagStart = $pos;
+		$tagEnd = $pos;
+		$length = strlen($string);
+
+		while($tagStart && $outString[$tagStart] != '<') {
+			$tagStart--;
+		}
+		while($tagEnd < $length && $outString[$tagEnd] != '>') {
+			$tagEnd++;
+		}
+		if($string[$tagStart] == '<' && isset($string[$tagEnd])) {
+			/*
+				Get string until tag starting position
+			*/
+			$outString = substr($string, 0, $tagStart);
+
+			/*
+				Replace token with text
+			*/
+
+			$outString .= $arg;
+
+			/*
+				Add the rest of the string
+			*/
+			$outString .= substr($string, $tagEnd + 1);
+		}
+		return $outString;
+	}
 	private function renderNamespace($element, $namespace) {
+		$data = $namespace['data'];
 		return $element;
 	}
 }
